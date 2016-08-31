21eecc0b62b9f999394bb9a0afcb0fef2db8d46c
alexr03 <alexredding180@gmail.com>
2016-08-31 17:30:01 +0100

    Added logging
    Added Logging

--- IsAbusing.cs
+++ IsAbusing.cs
@@ -12,6 +12,7 @@ using SDG.Unturned;
 using Steamworks;
 using Rocket.Unturned.Chat;
 using Rocket.Core.Logging;
+using System.IO;
 
 namespace IsAbusing
 {
@@ -19,12 +20,25 @@ namespace IsAbusing
     {
         public static IsAbusing Instance;
 
+        public string directory = System.IO.Directory.GetCurrentDirectory() + "/..";
+
         public static UnturnedPlayer murderer3;
 
         protected override void Load()
         {
             Instance = this;
             Rocket.Unturned.Events.UnturnedPlayerEvents.OnPlayerDeath += onplayerdeath;
+
+            Logger.Log("FUCKING LOCATION >> " + directory);
+
+            if (File.Exists(directory + "/Admin-Abuse.txt)"))
+            {
+
+            }
+            else
+            {
+                File.CreateText(directory + "/Admin-Abuse.txt");
+            }
         }
 
         protected override void Unload()
@@ -55,7 +69,14 @@ namespace IsAbusing
                         if (murderer3.GodMode == true)
                         {
                             UnturnedChat.Say(player.CharacterName + " Died by a player in godmode ABUSER: " + murderer3.CharacterName, UnturnedChat.GetColorFromName(Configuration.Instance.Color, Color.green));
+
+                        using (StreamWriter w = File.AppendText(directory + "/Admin-Abuse.txt"))
+                        {
+                            w.WriteLine(player.CharacterName + " Died by a abusive admin! ABUSER: " + murderer3.CharacterName + " Steam64ID: " + murderer3.CSteamID + "\n)");
+                            w.Close();
                         }
+
+                    }
                         else
                         {
                             Logger.Log(player.CharacterName + " Died by a player not in godmode");
